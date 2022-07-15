af0098fb5d567c8f34b958eb7934c88dbe868a79
user <Adinaytalalantbekova>
2022-07-15 10:18:51 +0600

    Initial commit

---
+++ homework_1/app.js
@@ -0,0 +1,26 @@
+const passportInput = document.querySelector(".passportInput")
+const passportCheck = document.getElementsByClassName(".passportCheck");
+const passportResult = document.querySelector(".passportResult");
+
+
+// const passportRegExp = /1 \d(13)/;
+// passportCheck[0].addEventListener("click", () => {
+//     if (passportRegExp.test(passportInput.value)) {
+//         passportResult.innerText = "OK";
+//         passportResult.style.color = "yellow";
+//     } else {
+//         passportResult.innerText = "NOT OK";
+//         passportResult.style.color = "red";
+//     }
+// });
+
+const click =document.querySelector(".click")
+const block = document.querySelector(".block");
+
+let position = 0;
+click.addEventListener("click", () => {
+    position += 16;
+})
+
+
+
